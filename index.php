3a88741b26f3c8afd56ccfbf1c6ca1e1ebab1357
samarahwest <60265465+samarahwest@users.noreply.github.com>
2021-11-30 16:37:02 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,327 @@
+ <!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>My_Website1 || Homepage</title>
+<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.css">
+<script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="vendor/jquery/jquery.min.js"></script>
+<link rel="stylesheet" href="assets/css/ie10-viewport-bug-workaround.css">
+<script type="text/javascript" src="assets/js/ie-emulation-modes-warning.js"></script>
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+<nav style=" background-color:#002; min-height:800px; color:#FFF;">
+
+<div class="col-md-8">
+<ul class="pull-left nav-pills list-unstyled" style="margin-top:-20px;">
+<li style="font-family:'Trebuchet MS', Arial, Helvetica, sans-serif; font-size:18px; margin-top:33px;"><a style="color:#F70;">Westworld</a></li>
+<li><a href="#" style="color:#fff;">Widget</a></li>
+<li><a href="#" style="color:#fff;">Blog</a></li>
+<li><a href="#"  style="color:#fff;">Help Center</a></li>
+</ul>
+</div>
+<div class="col-md-2">
+<a href="#" class="btn" name="btn">Login</a>
+</div>
+<div class="col-md-2">
+<button class="btn" type="submit" style="border-radius:20px;">Signup</button>
+</div>
+
+<div class="col-md-12">
+<h1 style="margin-top:60px;font-size:40px; font-family:'Arial Black', Gadget, sans-serif" align="center">Upgrade your website <span style="color:#F90">without</span><span  style="color:#F70"> coding</span></h1>
+<p align="center">Elfsight widgets help 1,000,000+ website owners to increase sales, engage 
+</p>
+<p  align="center">
+visitors, collect leads and more. Save your time and money with ready-to-use 
+</p>
+<p  align="center">solutions and grow your business faster! ⚡️</p>
+</div>
+<div class="col-md-11" align="center" style="margin-top:30px;">
+<input type="text" class="form-control" placeholder="enter your email" style="width:300px; height:45px; border-radius:30px; ">
+</div>
+<div class="col-md-1" align="center">
+<button style="width:120px; height:45px; margin-left:-1250px; color:#FFF; background-color:#F06; border-radius:40px; margin-top:30px;">Start</button>
+</div>
+<div class="col-md-12">
+<h6 class="text-muted" align="center" style="margin-top:20px;">Get started for free. no credit card needed</h6>
+</div>
+<div class="col-md-12" align="center" style="margin-top:80px;">
+<img src="img/img2.PNG" alt="slide" class="img-rounded img-responsive" style="width:50%;">
+</div>
+</nav>
+<div class="container" style="min-height:300px;">
+<div class="col-md-4" style="margin-top:50px;">
+
+<h4><strong>
+Select the widget for your task
+</strong></h4>
+<h6>Choose a widget for any website goal. Increase visitor engagement, boost conversions, generate leads, provide customer support and grow social followers.</h6>
+</div>
+<div class="col-md-4"  style="margin-top:50px;">
+
+<h4><strong>
+Set up & adjust it without coding
+</strong></h4>
+<h6>Customize your widget with the help of our intuitive editor in a few clicks. Embed it to your website in a matter of seconds.</h6>
+</div>
+<div class="col-md-4"  style="margin-top:50px;">
+
+<h4><strong>
+Analyze the widget’s efficiency
+</strong></h4>
+<h6>Monitor your widget’s most important metrics, check how it does the job and see its true value for your website.</h6>
+</div>
+</div>
+<div>
+<div class="col-md-12" align="center">
+<h3><strong>
+Smart response to website efficiency challenges
+</strong>
+</h3>
+<h6>
+Save your time. Our widgets will deal with your website tasks for you.
+</h6>
+</div>
+<div class=" col-md-6 well" style="width:500px; min-height:400px;  margin-left:130px;" align="center">
+<h3><strong>
+Social
+</strong>
+</h3>
+<h6>
+Embed content from Instagram, Facebook, Twitter, Pinterest and other social media into your website.
+
+</h6>
+</div>
+<div class=" col-md-6 well" style="width:500px; min-height:400px; margin-left:40px;" align="center">
+<h3><strong>
+Reviews
+</strong>
+</h3>
+<h6>
+Display customer reviews from Facebook, Instagram, Google, Yelp, TripAdviser and more on your website.
+</h6>
+</div>
+<div class=" col-md-4 well" style=" width:300px; min-height:400px; margin-left:130px;" align="center">
+<h3><strong>
+Chats
+</strong>
+</h3>
+<h6>
+Let visitors chat with you via their favorite messengers (Facebook and WhatsApp) right on your website.
+</h6>
+</div>
+<div class=" col-md-4 well"  style=" width:300px; min-height:400px; margin-left:50px;"  align="center">
+<h3><strong>
+Forms
+</strong>
+</h3>
+<h6>
+Create a diversity of forms: contact, feedback, survey, quiz, questionnary and more for your website.
+</h6>
+</div>
+<div class=" col-md-4 well"  style=" width:300px; min-height:400px; margin-left:90px;"  align="center">
+<h3><strong>
+Tools
+</strong>
+</h3>
+<h6>
+Meet any website challenge with a diversity of ready solutions by Westworld. Save your time and money.
+</h6>
+</div>
+</div>
+<div class=" col-md-12 container-fluid" style=" background-color:#000; color:#FFF; width:100%;">
+<div class="col-md-6" style="margin-top:100px;">
+<h1 style="font-size:45px; margin-left:40px;">Trusted by 1,078,753
+customers in all the world</h1>
+<h6  style="margin-top:100px; margin-left:40px;">Mathias Gwadanican, brand strategist</h6>
+<h3 style="margin-left:40px;">“Elfsight widgets are amazing, and their support is real fast and kind. Relevant and reliable, I strongly recommend Westworld!”
+</h3>
+</div>
+
+<div class="col-md-6" style="min-height:500px;">
+<img src="animationslider/img1.png" style="width:50%;" class="img-circle">
+<img src="animationslider/img2.png" style="width:60%; margin-top:10px; margin-left:250px;  border-radius:80px;"  class="img-circle">
+<img src="animationslider/img3.png" style="width:70%;  margin-top:10px;  border-radius:40px; margin-left:206px;"  class="img-circle">
+</div>  
+</div>
+<div class="col-md-12" align="center">
+<h1 style="margin-top:50px; color:#000;">Our widgets work. Anywhere.</h1>
+<h5>Integrated with any website platform, no matter which is yours.</h5>
+
+</div>
+<div class="col-md-12" style="color:#009;" align="center">
+<ul class=" list-unstyled list-inline active container">
+<li><a href="#"><span class="glyphicon glyphicon-briefcase"></span>Wordpress</a></li>
+<li><a href="#"><span class="glyphicon glyphicon-calendar"></span>Shopify</a></li>
+<li><a href="#"><span class="glyphicon glyphicon-camera"></span>Weebly</a></li>
+<li><a href="#"><span class="glyphicon glyphicon-barcode"></span>BigCommerce</a></li>
+<li><a href="#"><span class="glyphicon glyphicon-book"></span>Squarespace</a></li>
+<li><a href="#"><span class="glyphicon glyphicon-bookmark"></span>Webflow</a></li>
+<li><a href="#"><span class="glyphicon glyphicon-chevron-up"></span>Wix</a></li>
+<li><a href="#"><span class="glyphicon glyphicon-check"></span>HTML</a></li> 
+<li><a href="#"><span class="glyphicon glyphicon-chevron-right"></span>Google Sites</a></li>
+</ul>
+</div>
+<div class="col-md-12"  style="color:#009;"  align="center">
+<ul class=" list-unstyled list-inline  container">
+<li><a href="#"><span class="glyphicon glyphicon-chevron-left"></span>OpenCart</a></li>
+<li><a href="#"><span class="glyphicon glyphicon-circle-arrow-down"></span>AdobeMuse</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-wrench"></span>Bitrix</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-bullhorn"></span>Bootstrap</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-warning-sign"></span>ExpressionEngine</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-certificate"></span>Cs Cart</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-tree-deciduous"></span>Dle</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-tree-conifer"></span>Mobirise</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-trash"></span>lightCMS</a></li>
+</ul>
+</div>
+<div class="col-md-12 list-unstyled list-inline"  style="color:#009;"  align="center">
+<ul class="list-unstyled list-inline  container">
+<li><a  href="#"><span class="glyphicon glyphicon-tasks"></span>Cratejoy</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-tags"></span>Elementor</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-subtitles"></span>Facebook Page</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-step-backward"></span>Joomla</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-stop"></span>jQuery</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-time"></span>Margento</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-tower"></span>Blogger</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-stats"></span>Drupal</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-sound-stereo"></span>3dCart</a></li>
+</ul>
+</div>
+<div class="col-md-12 list-unstyled list-inline"  style="color:#009;" align="center">
+<ul class="list-unstyled list-inline  container">
+<li><a  href="#"><span class="glyphicon glyphicon-transfer"></span>Big Cartel</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-star-empty"></span>Blogspot</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-sound-dolby"></span>Volution</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-tree-conifer"></span>Macaw</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-signal"></span>Ucoz</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-adjust"></span>Strikingly</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-signal"></span>Sitebuilder</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-share-alt"></span>Splash</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-send"></span>Pivot</a></li>
+</ul>
+</div>
+<div class="col-md-12 list-unstyled list-inline"  style="color:#009;"  align="center">
+<ul class="list-unstyled list-inline  container">
+<li><a  href="#"><span class="glyphicon glyphicon-phone"></span>Optimizepress</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-road"></span>Pinegrow</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-retweet"></span>Tilda</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-registration-mark"></span>WooCommerce</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-plus"></span>SiteOrigin</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-play-circle"></span>SP Page Builder</a></li>
+<li><a  href="#"><span class="glyphicon  glyphicon-off"></span>ModX</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-picture"></span>Webnode</a></li>
+</ul>
+</div>
+<div class="col-md-12"  style="color:#009;"  align="center">
+<ul class="list-unstyled list-inline  container">
+<li><a  href="#"><span class="glyphicon glyphicon-move"></span>Lightspeed</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-music"></span>Duda</a></li>
+<li><a  href="#"><span class="glyphicon glyphicon-record"></span>EcWid</a></li>
+<li><a  href="#">GoDaddy</a></li>
+
+</ul>
+</div>
+<div class="col-md-12" align="center" style="min-height:400px;background-color:#090; color:#FFF;">
+<h1 style="margin-top:100px; font-family:'Trebuchet MS', Arial, Helvetica, sans-serif;font-size:50px;">Give the green light to your website 
+</h1>
+<h1 style=" margin-top:-10px;font-family:'Trebuchet MS', Arial, Helvetica, sans-serif;font-size:50px;">and business growth!
+</h1>
+<button class="btn" type="submit" style="height:40px; border-radius:20px; width:150px; background-color:#000;">Sign up for free</button>
+</div>
+<div style="min-height:600px; background-color:#000; color:#fff; line-height:30px;">
+<div class="container">
+<div class="col-md-2 list-unstyled">
+<h5>Widget</h5>
+
+<li><a>Instagram Feed</a></li>
+<li><a>Google Reviews</a></li>
+<li><a>Whatsapp Chat</a></li>
+<li><a>Social Feed</a></li>
+<li><a>All-in-One-Chat</a></li>
+<li><a>Countdown Timer</a></li>
+<li><a>Popup</a></li>
+<li><a>Facebook Feed</a></li>
+<li><a>Youtube Gallery</a></li>
+<li><a>See all widgets</a></li>
+
+</div>
+<div class="col-md-2 list-unstyled">
+<h5>Platforms</h5>
+
+<li><a>WordPress</a></li>
+<li><a>Shopify</a></li>
+<li><a>Weebly</a></li>
+<li><a>BigCommerce</a></li>
+<li><a>Squarespace</a></li>
+<li><a>Webflow</a></li>
+<li><a>Wix</a></li>
+<li><a>HTML</a></li>
+<li><a>Google Sites</a></li>
+<li><a>See all platforms</a></li>
+
+</div>
+<div class="col-md-2 list-unstyled">
+<h5>Company</h5>
+
+<li><a>Careers</a></li>
+<li><a>Contact Us</a></li>
+<li><a>Pricing</a></li>
+<li><a>Affliate Program</a></li>
+<li><a>Developers</a></li>
+<h5>Community</h5>
+<li><a>Facebook</a></li>
+<li><a>Instagram</a></li>
+<li><a>Twitter</a></li>
+<li><a>Youtube</a></li>
+
+</div>
+<div class="col-md-3 list-unstyled">
+<h5>Support</h5>
+
+<li><a>Help Center</a></li>
+<li><a>CodeCanyon Plugins Help</a></li>
+<h5>Resources</h5>
+<li><a>Feature Wishlist</a></li>
+<li><a>Tutorials</a></li>
+<li><a>Integrations</a></li>
+<li><a>Alternatives</a></li>
+<li><a>Industries</a></li>
+
+</div>
+<div class="col-md-3 list-unstyled">
+<h5>Platforms</h5>
+
+<li><a>Webflow CMS API: integrations and Documentation</a></li>
+<li><a>How to get and use eBay API key</a></li>
+<li><a>see all posts</a></li>
+
+
+</div>
+</div>
+<div class="col-md-12" style="background-color:#000;">
+<div class="container">
+<br><br>
+<hr  class="container" style="color:#CCC;">
+<div class="col-md-4" style="color:#fff;">
+<h6 style="color:#fff;">2021&copy; Westworld</h6>
+</div>
+<div class="col-md-4">   
+<h6 style="color:#fff;">Terms Of Service</h6>
+</div>
+<div class="col-md-4">
+<h6 style="color:#fff;">Privacy Policy</h6>
+</div>
+<div class="col-md-6 pull-right">
+<h6 class="pull-right"><span class="glyphicon glyphicon-globe"></span>english<span class="caret"></span></h6>
+</div>-->
+</div>
+</div>
+</div>
+<script type="text/javascript" src="assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
